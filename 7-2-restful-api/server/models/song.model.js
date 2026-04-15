ba7373edef6e8ee71bdf8c715ede37a0d086570c
suleiman-MBS <149168290+suleiman-MBS@users.noreply.github.com>
2026-04-15 09:16:22 +0300

    Add files via upload

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-// db schema
 const songSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  artist: { type: String, required: true }
-});
+  title:  { type: String, required: true, trim: true },
+  artist: { type: String, required: true, trim: true },
+  year:   { type: Number, min: 1000, max: 2026 } 
+}, { timestamps: true });
 
 export const Song = mongoose.model("Song", songSchema);
